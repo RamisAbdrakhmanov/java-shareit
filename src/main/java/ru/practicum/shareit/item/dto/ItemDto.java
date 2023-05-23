6706192ad409e6d666ad5ac46b7ddc783153a17a
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-05-23 13:03:28 +0300

    sprint 13 1.1.1

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,34 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+
+    Integer id;
+    @NotBlank(message = "Item name cannot be empty.")
+    String name;
+    @NotBlank(message = "Item description cannot be empty.")
+    @Size(max = 200, message = "Item description must be less than 200 characters.")
+    String description;
+    @NotNull(message = "Available is null")
+    Boolean available;
+    Integer ownerId;
+
+    public ItemDto(String name, String description, Boolean isAvailable, Integer integer) {
+    }
 }
