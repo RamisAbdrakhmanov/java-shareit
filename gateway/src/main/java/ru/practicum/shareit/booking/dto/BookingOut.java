5e0f9d0c0e269781cff551cc9e132736a7d59344
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-06-28 21:56:22 +0300

    style

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingOut.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemB;
+import ru.practicum.shareit.user.dto.UserB;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingOut {
+    private Integer id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Status status;
+    private UserB booker;
+    private ItemB item;
+}
