9e3974001a32b31ebf30a9a9a3a8d3e2ec9701d2
RamisAbdrakhmanov <ramis-abdr@yandex.ru>
2023-06-28 20:14:58 +0300

    style

---
+++ gateway/src/main/java/ru/practicum/shareit/exception/ElementException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class ElementException extends RuntimeException {
+    public ElementException(String message) {
+        super(message);
+    }
+}
--- server/src/main/java/ru/practicum/shareit/item/dto/ItemOwner.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemOwner.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,9 +18,7 @@ public class ItemOwner {
     private String name;
     private String description;
     private Boolean available;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private BookingIt lastBooking;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private BookingIt nextBooking;
     private List<CommentDto> comments;
 
